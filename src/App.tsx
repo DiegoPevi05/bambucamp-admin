d485e815c06e4216395975885f4ad1420eecb22b
diegopevi05 <diego10azul@hotmail.com>
2024-09-20 09:13:36 -0500

    see if its works

--- src/App.tsx
+++ src/App.tsx
@@ -157,7 +157,7 @@ const App: React.FC = () => {
     <Suspense fallback={<LoadingComponent isLoading={true} />}>
       <AuthProvider>
         <Providers>
-          <Router basename="/admin-page">
+          <Router basename="/">
             <AppRoutes />
           </Router>
         </Providers>
